9557afa7c3fabaeecbede64f8d5d88279678fb0a
gottaegbert <gottaegbert@gmail.com>
2021-10-26 00:32:53 +0800

    first commit

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,95 @@
+// briggs: EI, SN, TF, JP
+// color: brown, green, blue, red
+// letter: a, b, c, d
+var quizQuestions = [
+    // question #1
+    {
+        question: 'I am task oriented in order to achieve certain goals.',
+        answers: [{
+                // briggs, color, letter
+                type: 'J,Brown,D',
+                content: 'YEP! THAT IS ME!'
+            },
+
+            {
+                type: 'J,Brown,D,No',
+                content: "NAH, THAT CAN'T BE ME!"
+            }
+        ]
+    },
+    // question #2
+    {
+        question: 'I get bored easily when disscussing abstract concepts.',
+        answers: [{
+                // briggs, color, letter
+                type: 'N,Green,C',
+                content: 'YEP! THAT IS ME!'
+            },
+            {
+                type: ' ',
+                content: 'I HAVE NO IDEA!'
+            },
+            {
+                type: 'N,Green,C,No',
+                content: "NAH, THAT CAN'T BE ME!"
+            }
+        ]
+    },
+    // question #3
+    {
+        question: 'I like to try things out myself.',
+        answers: [{
+                // briggs, color, letter
+                type: 'S,Red,A',
+                content: 'YEP! THAT IS ME!'
+            },
+            {
+                type: ' ',
+                content: 'I HAVE NO IDEA!'
+            },
+            {
+                type: 'S,Red,A,No',
+                content: "NAH, THAT CAN'T BE ME!"
+            }
+        ]
+    },
+    // question #4
+    {
+        question: "I like to know where I'm going before I leave the house.",
+        answers: [{
+                // briggs, color, letter
+                type: 'J,Yellow,D',
+                content: 'YEP! THAT IS ME!'
+            },
+            {
+                type: ' ',
+                content: 'I HAVE NO IDEA!'
+            },
+            {
+                type: 'J,Yellow,D,No',
+                content: "NAH, THAT CAN'T BE ME!"
+            }
+        ]
+    },
+    // question #5
+    {
+        question: 'I believe there is a logical explanation for everything.',
+        answers: [{
+                // briggs, color, letter
+                type: 'T,Pink,C',
+                content: 'YEP! THAT IS ME!'
+            },
+            {
+                type: ' ',
+                content: 'I HAVE NO IDEA!'
+            },
+            {
+                type: 'T,Pink,C,No',
+                content: "NAH, THAT CAN'T BE ME!"
+            }
+        ]
+    },
+
+]
+
+export default quizQuestions
